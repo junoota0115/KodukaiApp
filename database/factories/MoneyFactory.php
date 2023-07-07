cf4e25a9b9f16752a6c51229a7abfdf41c620ff8
junoota0115 <j1u1n5@gmail.com>
2023-07-07 18:42:43 +0900

    SeederとFactoryの作成

---
+++ database/factories/MoneyFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Money;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+
+class MoneyFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
