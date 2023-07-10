1d14b2f1ec3a0c94cb854ee519782804587a3902
junoota0115 <j1u1n5@gmail.com>
2023-07-10 17:57:33 +0900

    User情報のfaker作成

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -3,18 +3,18 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\User;
 
-
-class DatabaseSeeder extends Seeder
+class UserSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Run the database seeds.
      *
      * @return void
      */
     public function run()
     {
-        \App\Models\User::factory(10)->create();
-
+        //
+        User::factory()->count(10)->create();
     }
 }
