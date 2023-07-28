ca5033e2a2e352fa0bf5c927880a152bc489613e
junoota0115 <j1u1n5@gmail.com>
2023-07-28 18:41:22 +0900

    アイコンの押下時変更

--- public/js/click.js
+++ public/js/click.js
@@ -19,3 +19,21 @@ $.ajax({
 
 });
 });
+
+function toggleIcon() {
+    var button = document.getElementById("hyouji");
+    var icon = button.querySelector("i");//buttonという変数に代入された要素内で最初に見つかる<i>要素を取得。
+    
+// アイコンのクラスを切り替える
+if (icon.classList.contains("fa-eye")) {//icon要素のクラスリストに"fa-eye"というクラスが含まれているかを確認するメソッド
+    icon.classList.remove("fa-eye");//もし含まれていれば削除
+    icon.classList.add("fa-eye-slash");//fa-eye-slashをその位置に追加
+  } else {
+    icon.classList.remove("fa-eye-slash");
+    icon.classList.add("fa-eye");
+  }
+}
+
+// ボタン要素を取得し、クリックイベントにtoggleIcon関数を割り当てる
+var buttonElement = document.getElementById("hyouji");
+buttonElement.addEventListener("click", toggleIcon);
