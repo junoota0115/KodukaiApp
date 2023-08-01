139a68a77b4c0a6c44d9e731d4bd53fc673a4dbf
junoota0115 <j1u1n5@gmail.com>
2023-08-01 17:59:32 +0900

    tableのbootstrap使用

--- resources/views/money/detail.blade.php
+++ resources/views/money/detail.blade.php
@@ -7,22 +7,29 @@
 
 <div class="money-log-main">
     <div class="money-log">
-        <table>
-            <th>入金日/入金時間</th>
-            <th>｜金額</th>
-            <th>｜コメント</th>
+        <table class="table table-striped">
+            <thead>
+                <tr>
+                    <th>#</th>
+                    <th>入金日 / 入金時間</th>
+                    <th>｜金額</th>
+                    <th>｜コメント</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach($money_logs as $money_log)
+                
+                <tr>
+                    <td>{{$money_log->id}}</td>
+                    <td>{{$money_log->created_at}}</td>
+                    <td>{{$money_log->price}}円</td>
+                    <td>{{$money_log->comment}}</td>
+                </tr>
+                
+                @endforeach
+            </tbody>
         </table>
-        @foreach($money_logs as $money_log)
-        <table>
-        <div class="money-log">
-            <td>{{$money_log->created_at}}</td>
-            <td>{{$money_log->price}}円</td>
-            <td>{{$money_log->comment}}</td>
-        </div>
-    </table>
-        @endforeach
-
-
+        
         <a href="{{ url('/create') }}" class="btn btn-secondary"><i class="fa-solid fa-money-bill-transfer"></i>入金</a>
         <a href="{{ url('/') }}" class="btn btn-secondary"><i class="fa-solid fa-house"></i>戻る</a>
 
